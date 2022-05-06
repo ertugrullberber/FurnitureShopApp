8e0c126891b6b778d94ea48cee10217ffa9f02f8
ertugrullberber <ertugrulberber3461@gmail.com>
2022-05-06 20:48:32 +0300

    Initial Commit

---
+++ furnitureShopApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  furnitureShopApp
+//
+//  Created by Ertugrul Berber on 6.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ furnitureShopApp/furnitureShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  furnitureShopAppApp.swift
+//  furnitureShopApp
+//
+//  Created by Ertugrul Berber on 6.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct furnitureShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
